1c66314845367895b35733d472dd34a2c9ddd44e
Kadir <admh.gspb@gmail.com>
2024-07-18 12:42:43 +0200

    modif lien du footer

--- footer.php
+++ footer.php
@@ -3,7 +3,8 @@
         <nav class="footer-navigation">
             <ul>
                 <li><a href="<?php echo home_url('/mentions-legales'); ?>">Mentions légales</a></li>
-                <li><a href="<?php echo home_url('/vie-privee'); ?>">Vie privée</a></li>
+                <li><a href="<?php echo get_privacy_policy_url(); ?>">Vie privée</a></li>
+
             </ul>
         </nav>
         <p class="rights">Tous droits réservés</p>
